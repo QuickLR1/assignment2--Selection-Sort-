26d35470268d7206ae70b07a59565e7674c26766
QuickLR1l <bekaisain68@gmail.com>
2025-10-05 06:59:43 +0500

    feat(metrics): реализован класс PerformanceTracker для измерения производительности

--- src/main/java/metrics/PerformanceTracker.java
+++ src/main/java/metrics/PerformanceTracker.java
@@ -1,4 +1,61 @@
 package metrics;
 
 public class PerformanceTracker {
+    private long comparisons;
+    private long swaps;
+    private long arrayAccesses;
+    private long startTime;
+    private long endTime;
+
+    public void start() {
+        reset();
+        startTime = System.nanoTime();
+    }
+
+    public void stop() {
+        endTime = System.nanoTime();
+    }
+
+    public void incrementComparisons() {
+        comparisons++;
+    }
+
+    public void incrementSwaps() {
+        swaps++;
+    }
+
+    public void incrementArrayAccesses() {
+        arrayAccesses++;
+    }
+
+    public long getComparisons() {
+        return comparisons;
+    }
+
+    public long getSwaps() {
+        return swaps;
+    }
+
+    public long getArrayAccesses() {
+        return arrayAccesses;
+    }
+    public long getExecutionTime() {
+        return endTime - startTime;
+    }
+
+    public void reset() {
+        comparisons = 0;
+        swaps = 0;
+        arrayAccesses = 0;
+        startTime = 0;
+        endTime = 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Сравнений: %d | Обменов: %d | Обращений к массиву: %d | Время: %.3f мс",
+                comparisons, swaps, arrayAccesses, getExecutionTime() / 1_000_000.0
+        );
+    }
 }
